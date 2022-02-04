8d3f207e416f8589f4b9508d065fcd2be1f6ca37
Duedot43 <jackgendill@icloud.com>
2022-02-04 11:44:37 -0700

    Harry Potter?!

--- Python/Unit 4/Harry_Potter.py
+++ Python/Unit 4/Harry_Potter.py
@@ -9,7 +9,7 @@ def listToString(s):
     
     # traverse in the string  
     for ele in s: 
-        str1 += ele  
+        str1 += str(ele)
     
     # return string  
     return str1
@@ -33,10 +33,55 @@ for x in moneylst:
 centmath = listToString(cent)
 cantmath = int(centmath)
 quarters = float(cantmath)/float(25)
+quarters = str(quarters)
 quarterslst = convert(quarters)
 qrt = []
 for x in quarterslst:
     if str(x) == str("."):
         break
-    if dot != 1:
-        qrt.append(int(x))
+
+    qrt.append(int(x))
+count = 0
+newmon = cantmath
+while True:
+    if int(listToString(qrt)) == 0:
+        break
+    count = count+1
+    newmon = newmon-25
+    if int(count) == int(listToString(qrt)):
+        break
+dime = int(newmon)/10
+dimestr = str(dime)
+dimelst = convert(dimestr)
+dim = []
+for x in dimelst:
+    if str(x) == str("."):
+        break
+
+    dim.append(int(x))
+count = 0
+while True:
+    if int(listToString(dim)) == 0:
+        break
+    count = count+1
+    newmon = newmon-10
+    if int(count) == int(listToString(dim)):
+        break
+nickle = int(newmon)/5
+nicklestr = str(nickle)
+nicklelst = convert(nicklestr)
+nick = []
+for x in nicklelst:
+    if str(x) == str("."):
+        break
+
+    nick.append(int(x))
+count = 0
+while True:
+    if int(listToString(nick)) == 0:
+        break
+    count = count+1
+    newmon = newmon-5
+    if int(count) == int(listToString(nick)):
+        break
+print(f"{listToString(dol)} Dollors {listToString(qrt)} Quarters {listToString(dim)} Dimes {listToString(nick)} Nickles {newmon} Pennies")
