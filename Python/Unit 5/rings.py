5d12391e0005e49913dfea4b5e2db3beecc74bf7
Duedot43 <jackgendill@icloud.com>
2022-02-07 09:46:00 -0700

    rings

---
+++ Python/Unit 5/rings.py
@@ -0,0 +1,97 @@
+import turtle as tur
+import time
+
+#1st cir#
+
+tur.down()
+tur.circle(20)
+tur.up()
+tur.goto(0,-5)
+tur.down()
+tur.circle(25)
+
+#1st cir#
+
+#repos#
+
+tur.up()
+tur.goto(20,-10)
+tur.down()
+
+#repos#
+
+#2nd cir#
+
+tur.circle(20)
+tur.up()
+tur.goto(20,-15)
+tur.down()
+tur.circle(25)
+
+#2nd cir#
+
+#repos#
+
+tur.up()
+tur.goto(50,0)
+tur.down()
+
+#repos#
+
+#3rd cir#
+
+tur.down()
+tur.circle(20)
+tur.up()
+tur.goto(50,-5)
+tur.down()
+tur.circle(25)
+
+#3rd cir#
+
+
+
+#repos#
+
+tur.up()
+tur.goto(80,-10)
+tur.down()
+
+#repos#
+
+#3rd cir#
+
+tur.down()
+tur.circle(20)
+tur.up()
+tur.goto(80,-15)
+tur.down()
+tur.circle(25)
+
+#3rd cir#
+
+
+
+#repos#
+
+tur.up()
+tur.goto(80+30,0)
+tur.down()
+
+#repos#
+
+#3rd cir#
+
+tur.down()
+tur.circle(20)
+tur.up()
+tur.goto(80+30,-5)
+tur.down()
+tur.circle(25)
+
+#3rd cir#
+
+count = 0
+while count != 5:
+    time.sleep(1)
+    count = count+1
