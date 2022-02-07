b79270db7e4dad1d263bf9e99574b096c5bd9799
Duedot43 <jackgendill@icloud.com>
2022-02-07 08:52:38 -0700

    Tetris pieces

---
+++ Python/Unit 5/tetris.py
@@ -0,0 +1,46 @@
+import turtle as tur
+import time
+tur.down()
+tur.forward(20)
+tur.right(90)
+tur.forward(20)
+tur.left(90)
+tur.forward(20*3)
+tur.right(90)
+tur.forward(20)
+tur.right(90)
+tur.forward(20*4)
+tur.right(90)
+tur.forward(20*2)
+
+#another#
+
+tur.up()
+tur.goto(30,40)
+tur.right(90)
+tur.down()
+count = 0
+while count != 4:
+    tur.down()
+    tur.forward(20*2)
+    tur.left(90)
+    count = count+1
+
+#another#
+
+tur.up()
+tur.goto(-40,40)
+tur.right(90*3)
+tur.down()
+tur.forward(20*4)
+tur.left(90)
+tur.forward(20)
+tur.left(90)
+tur.forward(20*4)
+tur.left(90)
+tur.forward(20)
+
+count = 0
+while count != 5:
+    time.sleep(1)
+    count = count+1
