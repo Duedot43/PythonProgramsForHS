198b36f5e1d7e6addfdeb05aba14307bc2b59b05
Duedot43 <jackgendill@icloud.com>
2022-02-07 08:42:53 -0700

    Ay give me a minute i have done turtle in a while

--- Python/Unit 5/shapr_Turtle.py
+++ Python/Unit 5/shapr_Turtle.py
@@ -1,10 +1,39 @@
 import turtle as tur
 import time
 count = 0
-while count != 5:
+while count != 4:
     tur.down()
     tur.forward(20)
     tur.left(90)
+    count = count+1
+count = 0
+tur.up()
+tur.goto(30,30)
+tur.down()
+while count != 3:
+    tur.down()
+    tur.forward(20)
+    tur.left(60*2)
+    count = count+1
+count = 0
+tur.up()
+tur.goto(-30,30)
+tur.down()
+while count != 3:
+    tur.down()
+    tur.forward(20)
+    tur.left(60*2)
+    count = count+1
+
+count = 0
+tur.up()
+tur.goto(-15,-30)
+tur.down()
+tur.right(90/2)
+while count != 20:
+    tur.down()
+    tur.forward(3)
+    tur.left(5)
     count = count+1
 ###keep the window open for 5 seconds###
 count = 0
