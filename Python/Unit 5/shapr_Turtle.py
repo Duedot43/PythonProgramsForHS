f25d0524155b6a1300060f70f4d8cadbd375029c
Duedot43 <jackgendill@icloud.com>
2022-02-07 08:34:45 -0700

    Turtle!!

---
+++ Python/Unit 5/shapr_Turtle.py
@@ -0,0 +1,13 @@
+import turtle as tur
+import time
+count = 0
+while count != 5:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+###keep the window open for 5 seconds###
+count = 0
+while count != 5:
+    time.sleep(1)
+    count = count+1
