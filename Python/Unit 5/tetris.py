c85b816addc8298e91d59dcbbd007f8ae58a4139
Duedot43 <jackgendill@icloud.com>
2022-02-09 15:14:17 -0700

    tetris

--- Python/Unit 5/tetris.py
+++ Python/Unit 5/tetris.py
@@ -1,17 +1,49 @@
 import turtle as tur
 import time
 tur.down()
+\
+tur.forward(20)
+tur.right(90)
+tur.forward(20)
+tur.right(90)
 tur.forward(20)
 tur.right(90)
 tur.forward(20)
 tur.left(90)
-tur.forward(20*3)
+tur.forward(20)
+tur.left(90)
+tur.forward(20)
+tur.left(90)
+tur.forward(20)
+tur.left(180)
+tur.forward(20)
+tur.left(90)
+tur.forward(20*4)
+tur.left(90)
+tur.forward(20)
+tur.left(90)
+tur.forward(20)
+tur.left(90)
+tur.forward(20)
 tur.right(90)
 tur.forward(20)
 tur.right(90)
-tur.forward(20*4)
+tur.forward(20)
+tur.left(90)
+tur.forward(20)
+tur.left(90)
+tur.forward(20)
 tur.right(90)
-tur.forward(20*2)
+tur.forward(20)
+tur.right(90)
+tur.forward(20)
+tur.right(90)
+tur.forward(20*3)
+
+
+
+
+
 
 #another#
 
@@ -22,23 +54,69 @@ tur.down()
 count = 0
 while count != 4:
     tur.down()
-    tur.forward(20*2)
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+count = 0
+tur.right(90)
+
+#tur.forward(20)
+while count != 4:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+count = 0
+tur.right(90)
+#tur.forward(20)
+while count != 4:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+count = 0
+tur.right(90)
+#tur.forward(20)
+while count != 4:
+    tur.down()
+    tur.forward(20)
     tur.left(90)
     count = count+1
 
 #another#
 
-tur.up()
-tur.goto(-40,40)
-tur.right(90*3)
-tur.down()
-for x in range(0,2):
-    tur.left(90)
-    tur.forward(20*4)
-    tur.left(90)
-    tur.forward(20)
 
+#another#
+tur.up()
+tur.goto(90,90)
+tur.down()
+#tur.goto(30,40)
+tur.right(90)
+tur.down()
 count = 0
-while count != 5:
-    time.sleep(1)
+while count != 4:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+count = 0
+tur.forward(20)
+while count != 4:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+count = 0
+tur.forward(20)
+while count != 4:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
+    count = count+1
+count = 0
+tur.forward(20)
+while count != 4:
+    tur.down()
+    tur.forward(20)
+    tur.left(90)
     count = count+1
