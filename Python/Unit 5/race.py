9d6b5fed303ded66de70aa89d74f27651cfaac13
Duedot43 <jackgendill@icloud.com>
2022-02-15 14:39:22 -0700

    yes

---
+++ Python/Unit 5/race.py
@@ -0,0 +1,32 @@
+import turtle as tur
+tur.up()
+#tur.left(90)
+tur.forward(100)
+tur.right(90)
+tur.down()
+tur.forward(75)
+tur.right(90)
+tur.up()
+tur.forward(200)
+tur.down()
+tur.right(90)
+tur.forward(75)
+tur.right(90)
+tur.up()
+tur.forward(200)
+tur.right(90)
+tur.forward(75/2)
+tur.right(90)
+tur.up()
+tur.goto(900,900)
+
+##line##
+
+t1 = tur.Turtle()
+t1.up()
+t1.forward(100)
+t1.right(90)
+forw = 75/4
+t1.forward(forw)
+t1.right(90)
+t1.down()
---
+++ Python/Unit 5/user_sides.py
@@ -0,0 +1,26 @@
+import turtle as tur
+game = 1
+while game == 1:
+    n = int(input("How many sides would you like?\n"))
+    while n < 2:
+        print("Invalid!")
+        n = int(input("How many sides would you like?\n"))
+    angle = ((n-2)*180)/n
+    print(angle)
+    count = 0
+    while count != n:
+        count = count+1
+        tur.left(180-angle)
+        tur.forward(50)
+    yes = 0
+    while yes == 0:
+        again = str(input("Would you like to play again? \n(Y) Yes\n(N) No\n"))
+        if again.upper() == "Y":
+            print("Resseting program...")
+            tur.clear()
+            yes = 1
+            game = 1
+        if again.upper() == "N":
+            exit()
+        if again.upper() != "Y" and again.upper() != "N":
+            print("Invalid!")
