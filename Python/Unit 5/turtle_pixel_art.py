bc3c2cb93be77db048e0210c999e1a24d636ef5d
Duedot43 <jackgendill@icloud.com>
2022-03-01 14:55:00 -0700

    Pixel art

--- Python/Unit 5/turtle_pixel_art.py
+++ Python/Unit 5/turtle_pixel_art.py
@@ -7,15 +7,144 @@ def square(color):
         tur.right(90)
     tur.left(90)
     tur.end_fill()
-tur.up()
-tur.left(90)
-tur.forward(200)
-tur.left(90)
-tur.forward(100)
-tur.down()
-for i in range(0,5):
-    tur.forward(200)
-    tur.left(90)
-tur.left(90)
-square("red")
-square("red")
+def reset():
+    tur.right(90)
+    tur.forward(20)
+    tur.right(90)
+    tur.forward(20*10)
+    tur.right(180)
+#tur.up()
+#tur.left(90)
+#tur.forward(200)
+#tur.left(90)
+#tur.forward(100)
+#tur.down()
+#for i in range(0,5):
+#    tur.forward(200)
+#    tur.left(90)
+#tur.left(90)
+tur.goto(0,100)
+tur.speed(500)
+for t in range(0,7):
+    for x in range(0,10):
+        square("#1a1a1a")
+    tur.right(90)
+    tur.forward(20)
+    tur.right(90)
+    tur.forward(20*10)
+    tur.right(180)
+for y in range(0,2):
+    for x in range(0,5):
+        square("#1a1a1a")
+    square("#FEBE23")
+    square("#FEBE23")
+    for x in range(0,3):
+        square("#1a1a1a")
+    tur.right(90)
+    tur.forward(20)
+    tur.right(90)
+    tur.forward(20*10)
+    tur.right(180)
+for t in range(0,1):
+    for x in range(0,10):
+        square("#1a1a1a")
+    tur.right(90)
+    tur.forward(20)
+    tur.right(90)
+    tur.forward(20*10)
+    tur.right(180)
+for y in range(0,2):
+    square("#FEBE23")
+    square("#FEBE23")
+    for x in range(0,8):
+        square("#1a1a1a")
+    tur.right(90)
+    tur.forward(20)
+    tur.right(90)
+    tur.forward(20*10)
+    tur.right(180)
+square("#1a1a1a")
+square("#AD2889")
+for x in range(0,5):
+    square("#1a1a1a")
+square("#E55B03")
+for x in range(0,2):
+    square("#1a1a1a")
+tur.right(90)
+tur.forward(20)
+tur.right(90)
+tur.forward(20*10)
+tur.right(180)
+for x in range(0,3):
+    square("#AE2887")
+for x in range(0,2):
+    square("#1a1a1a")
+for x in range(0,3):
+    square("#E45A06")
+square("#0C9BD8")
+square("#1a1a1a")
+reset()
+square("#0C9BD8")
+for x in range(0,3):
+    square("#e55b03")
+square("#1a1a1a")
+square("#1F41C6")
+square("#FEBE23")
+square("#FEBE23")
+square("#1099D8")
+#square("#0C9BD8")
+square("#1a1a1a")
+reset()
+square("#0E9AD5")
+square("#DF5C00")
+for t in range(0,2):
+    square("#59B200")
+square("#1a1a1a")
+square("#2042C2")
+square("#FEBE23")
+square("#FEBE23")
+square("#0E99D7")
+square("#1a1a1a")
+reset()
+square("#0E9AD5")
+for t in range(0,3):
+    square("#59B200")
+square("#2042C2")
+square("#2042C2")
+square("#D50E35")
+square("#D50E35")
+square("#0E9AD5")
+square("#1a1a1a")
+reset()
+square("#0E9AD5")
+square("#D90D31")
+square("#AA2987")
+for i in range(0,2):
+    square("#59B102")
+square("#1a1a1a")
+square("#2340C9")
+for i in range(0,2):
+    square("#D31036")
+square("#1a1a1a")
+reset()
+for i in range(0,2):
+    square("#FA2442")
+for i in range(0,2):
+    square("#AE2888")
+square("#5DB300")
+square("#E15D02")
+for x in range(0,3):
+    square("#2340C7")
+square("#1a1a1a")
+reset()
+square("#FA2442")
+square("#1a1a1a")
+square("#B1258D")
+for x in range(0,3):
+    square("#EA6910")
+for x in range(0,4):
+    square("#0F9BD6")
+import time
+time.sleep(20)
+
+
