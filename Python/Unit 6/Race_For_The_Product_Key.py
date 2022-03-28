0a1f95ae3985d086bbbe1ebaf6591a8eb4aef5cf
Duedot43 <jackgendill@icloud.com>
2022-03-28 15:27:57 -0600

    Just starting over

---
+++ Python/Unit 6/Race_For_The_Product_Key.py
@@ -0,0 +1,26 @@
+import os
+import random
+def mkboard():
+    pass
+def ckwin():
+    pass
+def drink():
+    pass
+def sleep():
+    pass
+def walk():
+    pass
+def sprint():
+    pass
+def run():
+    pass
+def setup():
+    global player_stats
+    global enemy_distance
+    player_stats = [0, 0, 5, 10]
+    enemy_distance = 0
+def choose():
+    pass
+game = 1
+while game == 1:
+    choose()
