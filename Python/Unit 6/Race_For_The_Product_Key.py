dc4780ac02ffa2ded64b468e894876a3dcd7c61c
Duedot43 <jackgendill@icloud.com>
2022-03-30 08:42:43 -0600

    EEEEEEEEEEE

--- Python/Unit 6/Race_For_The_Product_Key.py
+++ Python/Unit 6/Race_For_The_Product_Key.py
@@ -1,7 +1,9 @@
 import os
 import random
 def mkboard():
-    pass
+    global player_stats
+    global enemy_distance
+    print(f"Distance: {player_stats[0]} Thirst: {player_stats[1]} Drinks left: {player_stats[2]} Enegry: {player_stats[3]}\nEnemy distance: {enemy_distance}")
 def ckwin():
     pass
 def drink():
@@ -20,7 +22,23 @@ def setup():
     player_stats = [0, 0, 5, 10]
     enemy_distance = 0
 def choose():
-    pass
+    global player_stats
+    global enemy_distance
+    choice = int(input("(1) Drink water\n(2) Walk\n(3) Sprint\n(4) Run\n(5) Sleep for the night\n(6) Quit\n? "))
+    if choice == 1:
+        drink()
+    if choice == 2:
+        walk()
+    if choice == 3:
+        sprint()
+    if choice == 4:
+        run()
+    if choice == 5:
+        sleep()
+    if choice == 6:
+        exit()
+setup()
 game = 1
 while game == 1:
+    mkboard
     choose()
