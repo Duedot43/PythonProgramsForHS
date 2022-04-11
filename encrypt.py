997336578e9ba6441c8096c961d860321fe15abc
Duedot43 <jackgendill@icloud.com>
2022-04-11 13:05:24 -0600

    o

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,5 @@
 import os
 def encode(text):
-	os.system("mkdir /tmp/encode")
 	os.system("echo You spent way too much time on this >> /tmp/encode/orig")
 	os.system("echo '" + text + "' | basenc --base64 | basenc --base64url | basenc --base32 | basenc --base32hex | basenc --base16 | basenc --base2msbf | basenc --base2lsbf  | basenc --base64 | basenc --base64url | basenc --base32 | basenc --base32hex | basenc --base16 | basenc --base2msbf | basenc --base2lsbf  | basenc --base64 | basenc --base64url | basenc --base32 | basenc --base32hex | basenc --base16 | basenc --base2msbf | basenc --base2lsbf >> ./encrypted_file")
 def decode():
