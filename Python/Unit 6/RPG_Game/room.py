f7237a9e5f70b03dd4f986ab0119a022602acd28
Coeliman <coeliman24@gmail.com>
2022-04-18 14:46:37 -0600

    go pelase

---
+++ Python/Unit 6/RPG_Game/room.py
@@ -0,0 +1,54 @@
+import random,os,time
+#unlocked locations
+def spawn():
+    os.system("clear")
+    print("You wake up in a ditch, with farms surrounding you. You can feel a note inside of your pocket.")
+def SWoutpost():
+    os.system("clear")
+    outpostrand = random.randint(1,6)
+    if outpostrand == 1:
+        print("You wander into an outpost, it is filled with weapons and tools of war.")
+    elif outpostrand == 2:
+        print("You walk into an outpost, the eerie feeling makes you tingle.")
+    elif outpostrand == 3:
+        print("You sneak into an outpost, you wonder how soldiers survive in them.")
+    elif outpostrand == 4:
+        print("Whilst walking you happen upon an outpost, it sits as a tall tower above the land.")
+    elif outpostrand == 5:
+        print("You come across an outpost, it seems as if it is ready for war.")
+    elif outpostrand == 6:
+        print("")
+    
+def NEoutpost1():
+    os.system("clear")
+def NEoutpost2():
+    os.system("clear")
+def NWoutpost1():
+    os.system("clear")
+def NWoutpostocc():
+    os.system("clear")
+def SWbarn():
+    os.system("clear")
+def Sbarn():
+    os.system("clear")
+def SEbarn1():
+    os.system("clear")
+def SEbarn2():
+    os.system("clear")
+def Ebarn():
+    os.system("clear")
+def NEbarn():
+    os.system("clear")
+def NWbarn():
+    os.system("clear")
+def pier():
+    os.system("clear")
+def forest():
+    os.system("clear")
+def outlaw():
+    os.system("clear")
+def vinyard():
+    os.system("clear")
+def milbase():
+    os.system("clear")
+#locked locationss
