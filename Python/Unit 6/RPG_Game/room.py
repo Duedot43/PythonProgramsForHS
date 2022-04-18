7395d564fcd24f74618829be31e59bcfbcaa9a36
Coeliman <coeliman24@gmail.com>
2022-04-18 15:13:47 -0600

    numbero dos

--- Python/Unit 6/RPG_Game/room.py
+++ Python/Unit 6/RPG_Game/room.py
@@ -17,16 +17,67 @@ def SWoutpost():
     elif outpostrand == 5:
         print("You come across an outpost, it seems as if it is ready for war.")
     elif outpostrand == 6:
-        print("")
-    
+        print("You find an outpost, maybe the tools it holds could be of use.")
 def NEoutpost1():
     os.system("clear")
+    outpostrand = random.randint(1,6)
+    if outpostrand == 1:
+        print("You wander into an outpost, it is filled with weapons and tools of war.")
+    elif outpostrand == 2:
+        print("You walk into an outpost, the eerie feeling makes you tingle.")
+    elif outpostrand == 3:
+        print("You sneak into an outpost, you wonder how soldiers survive in them.")
+    elif outpostrand == 4:
+        print("Whilst walking you happen upon an outpost, it sits as a tall tower above the land.")
+    elif outpostrand == 5:
+        print("You come across an outpost, it seems as if it is ready for war.")
+    elif outpostrand == 6:
+        print("You find an outpost, maybe the tools it holds could be of use.")
 def NEoutpost2():
     os.system("clear")
+    outpostrand = random.randint(1,6)
+    if outpostrand == 1:
+        print("You wander into an outpost, it is filled with weapons and tools of war.")
+    elif outpostrand == 2:
+        print("You walk into an outpost, the eerie feeling makes you tingle.")
+    elif outpostrand == 3:
+        print("You sneak into an outpost, you wonder how soldiers survive in them.")
+    elif outpostrand == 4:
+        print("Whilst walking you happen upon an outpost, it sits as a tall tower above the land.")
+    elif outpostrand == 5:
+        print("You come across an outpost, it seems as if it is ready for war.")
+    elif outpostrand == 6:
+        print("You find an outpost, maybe the tools it holds could be of use.")
 def NWoutpost1():
     os.system("clear")
+    outpostrand = random.randint(1,6)
+    if outpostrand == 1:
+        print("You wander into an outpost, it is filled with weapons and tools of war.")
+    elif outpostrand == 2:
+        print("You walk into an outpost, the eerie feeling makes you tingle.")
+    elif outpostrand == 3:
+        print("You sneak into an outpost, you wonder how soldiers survive in them.")
+    elif outpostrand == 4:
+        print("Whilst walking you happen upon an outpost, it sits as a tall tower above the land.")
+    elif outpostrand == 5:
+        print("You come across an outpost, it seems as if it is ready for war.")
+    elif outpostrand == 6:
+        print("You find an outpost, maybe the tools it holds could be of use.")
 def NWoutpostocc():
     os.system("clear")
+    outpostrand = random.randint(1,6)
+    if outpostrand == 1:
+        print("You wander into an outpost, it is filled with weapons and tools of war.")
+    elif outpostrand == 2:
+        print("You walk into an outpost, the eerie feeling makes you tingle.")
+    elif outpostrand == 3:
+        print("You sneak into an outpost, you wonder how soldiers survive in them.")
+    elif outpostrand == 4:
+        print("Whilst walking you happen upon an outpost, it sits as a tall tower above the land.")
+    elif outpostrand == 5:
+        print("You come across an outpost, it seems as if it is ready for war.")
+    elif outpostrand == 6:
+        print("You find an outpost, maybe the tools it holds could be of use.")
 def SWbarn():
     os.system("clear")
 def Sbarn():
