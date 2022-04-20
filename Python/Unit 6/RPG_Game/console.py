d1b15a47b2c3ed3d32e252a987db188ae5cb2009
Duedot43 <jackgendill@icloud.com>
2022-04-20 14:39:50 -0600

    a

--- Python/Unit 6/RPG_Game/console.py
+++ Python/Unit 6/RPG_Game/console.py
@@ -1,11 +1,17 @@
 import get, goto, set
+def Convert(string):
+    li = list(string.split(" "))
+    return li
 def user_input(var_list, player_list):
     cpos = get.pos(player_list)
     ava = goto.disp(cpos)
-    usr_input = input("\n\n> ")
-    place_out = get.place(ava, usr_input)
-    if place_out[0] == 1:
-        player_list = set.pos(player_list, place_out)
-        return 0
-    else:
-        return 1
+    raw_raw_input = input("\n\n> ").lower()
+    raw_input = Convert(raw_raw_input)
+    if raw_input[0] == "walk" or raw_input[0] == "go" or raw_input[0] == "move" or raw_input[0] == "run":
+        usr_input = raw_input[1]
+        place_out = get.place(ava, usr_input)
+        if place_out[0] == 1:
+            player_list = set.pos(player_list, place_out)
+            return 0
+        else:
+            return 1
