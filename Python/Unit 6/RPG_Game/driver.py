b18efdd0e993a854606439abbd5e52fee7e63716
Coeliman <coeliman24@gmail.com>
2022-05-01 23:42:26 -0600

    beginning

--- Python/Unit 6/RPG_Game/driver.py
+++ Python/Unit 6/RPG_Game/driver.py
@@ -2,6 +2,18 @@ import get, roll, room, setup, set, goto, console
 game = 1
 var_list = setup.load_game()
 player_list = setup.load_player_info()
+print("————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————")
+print()
+print("Welcome to Aristocrat.")
+print()
+print("Aristocrat is a text based RPG, where you must save your family from the aristocrats of the city.")
+print("Every move you lose hunger.")
+print()
+print("Authorized commands in game: go, sell, loot, see, eat, blow up, and buy.")
+print("————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————")
+print()
+print("Enter to continue")
+input()
 while game == 1:
     yes = console.user_input(var_list, player_list)
     while yes != 0:
