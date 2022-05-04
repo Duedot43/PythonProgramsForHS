5f3847342b4c06ecd800c6d7a6d76ede4b757deb
Duedot43 <jackgendill@icloud.com>
2022-05-04 21:57:17 +0100

    Ba da!

--- RPI_Things/sequence.py
+++ RPI_Things/sequence.py
@@ -1,8 +1,31 @@
 from gpiozero import LED
 from time import sleep
-import random
-led1 = LED(4)
-led2 = LED()
-led3 = LED()
-led4 = LED()
-led5 = LED()
+from random import randint
+led1 = LED(5)
+led2 = LED(6)
+led3 = LED(13)
+led4 = LED(19)
+led5 = LED(26)
+try:
+    while True:
+        ledchoice = randint(1,5)
+        if ledchoice == 1:
+            theLED = led1
+        elif ledchoice == 2:
+            theLED = led2
+        elif ledchoice == 3:
+            theLED = led3
+        elif ledchoice == 4:
+            theLED = led4
+        elif ledchoice == 5:
+            theLED = led5
+        theLED.on()
+        sleep(.5)
+        theLED.off()
+except KeyboardInterrupt:
+    led1.off()
+    led2.off()
+    led3.off()
+    led4.off()
+    led5.off()
+    exit()
