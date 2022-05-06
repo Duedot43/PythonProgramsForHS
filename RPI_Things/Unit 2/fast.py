d0bdee00caf85a9c960a7a57e922bd9f6c38c723
Duedot43 <jackgendill@icloud.com>
2022-05-06 11:40:26 -0600

    Oh me meary

--- RPI_Things/Unit 2/fast.py
+++ RPI_Things/Unit 2/fast.py
@@ -2,32 +2,33 @@ from gpiozero import LED, Button
 import multiprocessing
 from time import sleep
 from random import randint
-led_start = LED()
-def button1s():
-    global led_start, button2m, win
-    button1 = Button(18)
-    led1 = LED(6)
-    button1.wait_for_press()
-    button2m.close()
-    led1.on()
-    led_start.off()
-    print("Player 1 wins!")
-    win = 0
-def button2s():
-    global led_start, button1m, win
-    button2 = Button(4)
-    led2 = LED(5)
-    button2.wait_for_press()
-    button1m.close()
-    led2.on()
-    led_start.off()
-    print("Player 2 wins!")
-    win = 1
-sleep(randint(2,4))
-led_start.on()
-button1m = multiprocessing.Process(target=button1s)
-button2m = multiprocessing.Process(target=button2s)
-button1m.start()
-button2m.start()
-button1m.join()
-button2m.join()
+while True:
+    led_start = LED()
+    def button1s():
+        global led_start, button2m, win
+        button1 = Button(18)
+        led1 = LED(6)
+        button1.wait_for_press()
+        button2m.close()
+        led1.on()
+        led_start.off()
+        print("Player 1 wins!")
+        win = 0
+    def button2s():
+        global led_start, button1m, win
+        button2 = Button(4)
+        led2 = LED(5)
+        button2.wait_for_press()
+        button1m.close()
+        led2.on()
+        led_start.off()
+        print("Player 2 wins!")
+        win = 1
+    sleep(randint(2,4))
+    led_start.on()
+    button1m = multiprocessing.Process(target=button1s)
+    button2m = multiprocessing.Process(target=button2s)
+    button1m.start()
+    button2m.start()
+    button1m.join()
+    button2m.join()
