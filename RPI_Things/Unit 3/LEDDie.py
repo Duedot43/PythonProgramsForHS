e32cf7d1cf6888fd4487579d3fe450135347e411
Duedot43 <jackgendill@icloud.com>
2022-05-16 11:00:32 -0600

    Fixed OG led die

--- RPI_Things/Unit 3/LEDDie.py
+++ RPI_Things/Unit 3/LEDDie.py
@@ -1,75 +1,81 @@
 from gpiozero import LED,Button
 from random import randint
 from time import sleep
-Button() = B
-LED() = TL
-LED() = TR
-LED() = L
-LED() = M
-LED() = R
-LED() = BL
-LED() = BR
-while:
-    B.wait_for_press()
-    for x in 5:
-        TL.off()
-        TR.off()
-        L.off()
-        M.off()
-        R.off()
-        BL.off()
-        BR.off()
+gpios = {
+    "button":[Button()],
+    "led":{
+        "TL":LED(),
+        "TR":LED(),
+        "L":LED(),
+        "M":LED(),
+        "R":LED(),
+        "BL":LED(),
+        "BR":LED()
+    }
+}
+while True:
+    gpios['button'][0].wait_for_press()
+    for x in gpios['led']:
+        gpios['led'][x].off()
+    for x in range(0,5):
+        gpios['led']['TL'].off()
+        gpios['led']['TR'].off()
+        gpios['led']['L'].off()
+        gpios['led']['M'].off()
+        gpios['led']['R'].off()
+        gpios['led']['BL'].off()
+        gpios['led']['BR'].off()
         sleep(0.2)
-        TL.on()
-        TR.on()
-        M.on()
-        BL.on()
-        BR.on()
+        gpios['led']['TL'].on()
+        gpios['led']['TR'].on()
+        gpios['led']['M'].on()
+        gpios['led']['BL'].on()
+        gpios['led']['BR'].on()
         sleep(0.2)
-        TL.off()
-        TR.off()
-        M.off()
-        BL.off()
-        BR.off()
+        gpios['led']['TL'].off()
+        gpios['led']['TR'].off()
+        gpios['led']['M'].off()
+        gpios['led']['BL'].off()
+        gpios['led']['BR'].off()
         sleep(0.2)
-        M.on()
-        R.on()
-        L.on()
+        gpios['led']['M'].on()
+        gpios['led']['R'].on()
+        gpios['led']['L'].on()
         sleep(0.2)
-        M.on()
-        R.on()
-        L.on()
+        gpios['led']['M'].on()
+        gpios['led']['R'].on()
+        gpios['led']['L'].on()
         sleep(0.2)
-        M.off()
-        R.off()
-        L.off()
-    randnum = random.randint(1,6)
+        gpios['led']['M'].off()
+        gpios['led']['R'].off()
+        gpios['led']['L'].off()
+    randnum = randint(1,6)
     if randnum == 1:
-        M.on()
+        gpios['led']['M'].on()
     elif randnum == 2:
-        TL.on()
-        BR.on()
+        gpios['led']['TL'].on()
+        gpios['led']['BR'].on()
     elif randnum == 3:
-        TL.on()
-        M.on()
-        BR.on()
+        gpios['led']['TL'].on()
+        gpios['led']['M'].on()
+        gpios['led']['BR'].on()
     elif randnum == 4:
-        TL.on()
-        TR.on()
-        BL.on()
-        BR.on()
+        gpios['led']['TL'].on()
+        gpios['led']['TR'].on()
+        gpios['led']['BL'].on()
+        gpios['led']['BR'].on()
     elif randnum == 5:
-        TL.on()
-        TR.on()
-        BL.on()
-        BR.on()
-        M.on()
+        gpios['led']['TL'].on()
+        gpios['led']['TR'].on()
+        gpios['led']['BL'].on()
+        gpios['led']['BR'].on()
+        gpios['led']['M'].on()
     elif randnum == 6:
-        TL.on()
-        TR.on()
-        L.on()
-        R.on()
-        M.on()
-        BR.on()
-        BL.on()
+        gpios['led']['TL'].on()
+        gpios['led']['TR'].on()
+        gpios['led']['L'].on()
+        gpios['led']['R'].on()
+        gpios['led']['M'].on()
+        gpios['led']['BR'].on()
+        gpios['led']['BL'].on()
         
